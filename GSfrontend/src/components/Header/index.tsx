7bc96aba1713b6e28629d0e28d9799336d65f5ff
Khanaccount <khanaccount1@mail.ru>
2023-10-31 04:14:57 +0700

    2

--- GSfrontend/src/components/Header/index.tsx
+++ GSfrontend/src/components/Header/index.tsx
@@ -1,30 +1,46 @@
 import React from "react";
+import { Link, useLocation } from "react-router-dom";
 
 import s from "./index.module.scss";
 
 const Header: React.FC = () => {
+	const location = useLocation();
+
 	return (
 		<div className={s.header}>
-			<img src="img/header/logo.svg" alt="logo" />
+			<Link to="/">
+				<img src="img/header/logo.svg" alt="logo" />
+			</Link>
 			<ul>
 				<li>
-					<a href="">Home</a>
+					<Link className={location.pathname === "/" ? s.active : ""} to="">
+						Home
+					</Link>
 				</li>
 				<li>
-					<a href="">Shop</a>
+					<Link className={location.pathname === "/shop" ? s.active : ""} to="">
+						Shop
+					</Link>
 				</li>
 				<li>
-					<a href="">Plant Care</a>
+					<Link className={location.pathname === "/plantCare" ? s.active : ""} to="">
+						Plant Care
+					</Link>
 				</li>
 				<li>
-					<a href="">Blogs</a>
+					<Link className={location.pathname === "/blogs" ? s.active : ""} to="">
+						Blogs
+					</Link>
 				</li>
 			</ul>
 			<div>
-				<img className={s.cart} width={24} height={24} src="img/header/cart.svg" alt="cart" />
+				<div className={s.cart}>
+					<img width={24} height={24} src="img/header/cart.svg" alt="cart" />
+					<span>6</span>
+				</div>
 
 				<button>
-					<img width={20} src="img/header/login.svg" alt="login" />
+					<img width={24} src="img/header/login.svg" alt="login" />
 					<p>Login</p>
 				</button>
 			</div>
--- GSfrontend/src/pages/Home.tsx
+++ GSfrontend/src/pages/Home.tsx
@@ -5,7 +5,7 @@ import Header from "../components/Header";
 
 export const Home: React.FC = () => {
 	return (
-		<div>
+		<div className="container">
 			<Header />
 		</div>
 	);
