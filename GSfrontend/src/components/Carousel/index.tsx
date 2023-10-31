fc031a83feb28a87397926318fa8ec49201b99a2
Khanaccount <khanaccount1@mail.ru>
2023-10-31 23:25:56 +0700

    4

--- GSfrontend/src/components/Carousel/index.tsx
+++ GSfrontend/src/components/Carousel/index.tsx
@@ -2,20 +2,74 @@ import React from "react";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from "react-responsive-carousel";
 
+import s from "./index.module.scss";
+
 const MyCarousel: React.FC = () => {
 	return (
-		<Carousel>
-			<div>
-				<img src="path_to_image_1.jpg" alt="Image 1" />
-				<p className="legend">Image 1</p>
+		<Carousel
+			className={s.Carousel}
+			showArrows={false}
+			showStatus={false}
+			showThumbs={false}
+			infiniteLoop={true}
+			autoPlay={true}
+			interval={5000}>
+			<div className={s.carouselFirst}>
+				<div className={s.carouselFirstLeft}>
+					<h5>Welcome to GreenShop</h5>
+					<h1>
+						Let’s Make a Better <span>planet</span>{" "}
+					</h1>
+					<p>
+						We are an online plant shop offering a wide range of cheap and trendy plants. Use our
+						plants to create an unique Urban Jungle. Order your favorite plants!
+					</p>
+					<button>
+						<a href="">SHOP NOW</a>
+					</button>
+				</div>
+				<div className={s.carouselFirstRight}>
+					<img className={s.plantBig} src="img/carousel/plant.png" alt="plant" />
+					<img className={s.plantSmall} src="img/carousel/plantSmall.png" alt="plant" />
+				</div>
 			</div>
-			<div>
-				<img src="path_to_image_2.jpg" alt="Image 2" />
-				<p className="legend">Image 2</p>
+			<div className={s.carouselFirst}>
+				<div className={s.carouselFirstLeft}>
+					<h5>Welcome to GreenShop</h5>
+					<h1>
+						Let’s Make a Better <span>planet</span>{" "}
+					</h1>
+					<p>
+						We are an online plant shop offering a wide range of cheap and trendy plants. Use our
+						plants to create an unique Urban Jungle. Order your favorite plants!
+					</p>
+					<button>
+						<a href="">SHOP NOW</a>
+					</button>
+				</div>
+				<div className={s.carouselFirstRight}>
+					<img className={s.plantBig} src="img/carousel/plant.png" alt="plant" />
+					<img className={s.plantSmall} src="img/carousel/plantSmall.png" alt="plant" />
+				</div>
 			</div>
-			<div>
-				<img src="path_to_image_3.jpg" alt="Image 3" />
-				<p className="legend">Image 3</p>
+			<div className={s.carouselFirst}>
+				<div className={s.carouselFirstLeft}>
+					<h5>Welcome to GreenShop</h5>
+					<h1>
+						Let’s Make a Better <span>planet</span>{" "}
+					</h1>
+					<p>
+						We are an online plant shop offering a wide range of cheap and trendy plants. Use our
+						plants to create an unique Urban Jungle. Order your favorite plants!
+					</p>
+					<button>
+						<a href="">SHOP NOW</a>
+					</button>
+				</div>
+				<div className={s.carouselFirstRight}>
+					<img className={s.plantBig} src="img/carousel/plant.png" alt="plant" />
+					<img className={s.plantSmall} src="img/carousel/plantSmall.png" alt="plant" />
+				</div>
 			</div>
 		</Carousel>
 	);
--- GSfrontend/src/pages/Home.tsx
+++ GSfrontend/src/pages/Home.tsx
@@ -1,12 +1,13 @@
 import React from "react";
-import Header from "../components/Header";
 
-// import Header from "../components/Header";
+import Header from "../components/Header";
+import Carousel from "../components/Carousel";
 
 export const Home: React.FC = () => {
 	return (
 		<div className="container">
 			<Header />
+			<Carousel />
 		</div>
 	);
 };
