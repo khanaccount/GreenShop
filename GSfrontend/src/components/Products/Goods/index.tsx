d53355037ca2e64e5d6560fc9c5e17331b00239a
Khanaccount <khanaccount1@mail.ru>
2023-11-02 01:09:55 +0700

    Подправил карточки

--- GSfrontend/src/components/Products/Goods/index.tsx
+++ GSfrontend/src/components/Products/Goods/index.tsx
@@ -11,7 +11,6 @@ type Goods = {
 	favoriteImg: string;
 	discountPercentage: string;
 	discount: boolean;
-	sale: boolean;
 	id: number;
 };
 
@@ -24,103 +23,94 @@ const items: Goods[] = [
 		mainImg: "img/goods/01.png",
 		cartImg: "img/goods/cart.svg",
 		favoriteImg: "img/goods/favorite.svg",
-		sale: true,
 		discount: false,
-		discountPercentage: "15"
+		discountPercentage: "11"
 	},
 	{
 		id: 2,
-		title: "Barberton Daisy",
-		mainPrice: "$119.00",
+		title: "Angel Wing Begonia",
+		mainPrice: "$169.00",
 		salePrice: "$229.00",
 		mainImg: "img/goods/02.png",
 		cartImg: "img/goods/cart.svg",
 		favoriteImg: "img/goods/favorite.svg",
-		sale: false,
 		discount: true,
-		discountPercentage: "15"
+		discountPercentage: "25"
 	},
 	{
 		id: 3,
-		title: "Barberton Daisy",
-		mainPrice: "$119.00",
+		title: "African Violet",
+		mainPrice: "$199.00",
 		salePrice: "$229.00",
 		mainImg: "img/goods/03.png",
 		cartImg: "img/goods/cart.svg",
 		favoriteImg: "img/goods/favorite.svg",
-		sale: false,
 		discount: false,
-		discountPercentage: "15"
+		discountPercentage: "12"
 	},
 	{
 		id: 4,
-		title: "Barberton Daisy",
-		mainPrice: "$119.00",
+		title: "Beach Spider Lily",
+		mainPrice: "$129.00",
 		salePrice: "$229.00",
 		mainImg: "img/goods/04.jpg",
 		cartImg: "img/goods/cart.svg",
 		favoriteImg: "img/goods/favorite.svg",
-		sale: false,
 		discount: true,
 		discountPercentage: "15"
 	},
 	{
 		id: 5,
-		title: "Barberton Daisy",
-		mainPrice: "$119.00",
+		title: "Blushing Bromeliad",
+		mainPrice: "$139.00",
 		salePrice: "$229.00",
 		mainImg: "img/goods/05.png",
 		cartImg: "img/goods/cart.svg",
 		favoriteImg: "img/goods/favorite.svg",
-		sale: false,
 		discount: false,
-		discountPercentage: "15"
+		discountPercentage: "5"
 	},
 	{
 		id: 6,
-		title: "Barberton Daisy",
-		mainPrice: "$119.00",
+		title: "Aluminum Plant",
+		mainPrice: "$179.00",
 		salePrice: "$229.00",
 		mainImg: "img/goods/06.png",
 		cartImg: "img/goods/cart.svg",
 		favoriteImg: "img/goods/favorite.svg",
-		sale: false,
 		discount: true,
-		discountPercentage: "15"
+		discountPercentage: "7"
 	},
 	{
 		id: 7,
-		title: "Barberton Daisy",
-		mainPrice: "$119.00",
+		title: "Bird's Nest Fern",
+		mainPrice: "$99.00",
 		salePrice: "$229.00",
 		mainImg: "img/goods/07.png",
 		cartImg: "img/goods/cart.svg",
 		favoriteImg: "img/goods/favorite.svg",
-		sale: false,
 		discount: false,
 		discountPercentage: "15"
 	},
 	{
 		id: 8,
-		title: "Barberton Daisy",
-		mainPrice: "$119.00",
+		title: "Broadleaf Lady Palm",
+		mainPrice: "$59.00",
 		salePrice: "$229.00",
 		mainImg: "img/goods/08.png",
 		cartImg: "img/goods/cart.svg",
 		favoriteImg: "img/goods/favorite.svg",
-		sale: false,
 		discount: true,
-		discountPercentage: "15"
+		discountPercentage: "11"
 	},
 	{
 		id: 9,
-		title: "Barberton Daisy",
-		mainPrice: "$119.00",
+		title: "Chinese Evergreen",
+		mainPrice: "$39.00",
 		salePrice: "$229.00",
 		mainImg: "img/goods/09.png",
 		cartImg: "img/goods/cart.svg",
 		favoriteImg: "img/goods/favorite.svg",
-		sale: false,
 		discount: false,
 		discountPercentage: "15"
 	}
@@ -148,7 +138,7 @@ const Goods: React.FC = () => {
 							<p className={s.goodsName}>{item.title}</p>
 							<div className={s.goodsPrices}>
 								<p className={s.main}>{item.mainPrice}</p>
-								{item.sale ? <p className={s.sale}>{item.salePrice}</p> : null}
+								{item.discount ? <p className={s.sale}>{item.salePrice}</p> : null}
 							</div>
 						</div>
 					</div>
