32d122dd39e3c45ba7a0e169d73665b97f809d94
Khanaccount <khanaccount1@mail.ru>
2023-11-17 14:56:01 +0700

    Добавил запрос в Register

--- GSfrontend/src/components/Auth/Register/index.tsx
+++ GSfrontend/src/components/Auth/Register/index.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import s from "./index.module.scss";
+import axios from "axios"; // Импортируем axios или другую библиотеку для отправки запросов
 
 interface RegisterProps {
 	passwordVisible: boolean;
@@ -7,17 +8,59 @@ interface RegisterProps {
 }
 
 const Register: React.FC<RegisterProps> = ({ passwordVisible, handleTogglePasswordVisibility }) => {
+	const [usernameOrEmail, setUsernameOrEmail] = useState("");
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [confirmPassword, setConfirmPassword] = useState("");
+
+	const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+
+		if (password !== confirmPassword) {
+			alert("Passwords do not match!");
+		} else {
+			try {
+				const response = await axios.post("http://localhost:8000/shop/registration/", {
+					usernameOrEmail,
+					email,
+					password
+				});
+				const token = response.data.token; // Предполагается, что сервер вернет токен в ответе
+				alert("Registration successful. Token received: " + token);
+				// Здесь можно сохранить токен в localStorage или использовать в приложении
+			} catch (error) {
+				console.error("Registration failed:", error);
+				alert("Registration failed. Please try again.");
+				// Обработка ошибок регистрации
+			}
+		}
+	};
+
 	return (
 		<div className={s.register}>
 			<p>Enter your email and password to register.</p>
-			<form className={s.form}>
-				<input type="text" name="usernameOrEmail" placeholder="Username" />
-				<input type="text" name="Email" placeholder="Enter your email address" />
+			<form className={s.form} onSubmit={handleSubmit}>
+				<input
+					type="text"
+					name="usernameOrEmail"
+					placeholder="Username"
+					value={usernameOrEmail}
+					onChange={(e) => setUsernameOrEmail(e.target.value)}
+				/>
+				<input
+					type="text"
+					name="Email"
+					placeholder="Enter your email address"
+					value={email}
+					onChange={(e) => setEmail(e.target.value)}
+				/>
 				<div className={s.passwordContainer}>
 					<input
 						type={passwordVisible ? "text" : "password"}
 						name="password"
 						placeholder="Password"
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
 					/>
 					<img
 						src={passwordVisible ? "img/header/eyeClose.svg" : "img/header/eye.svg"}
@@ -27,9 +70,11 @@ const Register: React.FC<RegisterProps> = ({ passwordVisible, handleTogglePasswo
 					/>
 				</div>
 				<input
-					type={passwordVisible ? "text" : "password"}
+					type={"password"}
 					name="сonfirmPassword"
 					placeholder="Confirm Password"
+					value={confirmPassword}
+					onChange={(e) => setConfirmPassword(e.target.value)}
 				/>
 				<button type="submit">Register</button>
 			</form>
