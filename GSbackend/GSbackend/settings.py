c22f30089d59c3e7aee517033ba9623568ab5fcc
HoverGo <kotikarbuz@gmail.com>
2023-12-11 21:55:51 +0500

    Исправил ошибку

--- GSbackend/GSbackend/settings.py
+++ GSbackend/GSbackend/settings.py
@@ -56,6 +56,9 @@ MIDDLEWARE = [
 ]
 
 REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.AllowAny",
+    ],
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
