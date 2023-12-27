dfacc19caf31a060e2623478b1e2a54dd0950e7c
Khanaccount <khanaccount1@mail.ru>
2023-12-28 00:21:03 +0700

    db.sqlite3

--- GSfrontend/src/components/Shop/Path/index.tsx
+++ GSfrontend/src/components/Shop/Path/index.tsx
@@ -1,31 +1,41 @@
 import React from "react";
-import { Link, useParams } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import s from "./index.module.scss";
 
 const Path: React.FC = () => {
-	const { id } = useParams<{ id: string }>();
-
+	const location = useLocation();
 	const pathItems = [
 		{ name: "Home", path: "/", isHome: true },
-		{ name: "Shop", path: "/" },
-		{ name: "Cart", path: "/Shop/Cart" }
+		{ name: "Shop", path: "/Shop" },
+		{ name: "Cart", path: "/Shop/Cart" },
+		{ name: "Checkout", path: "/Shop/Checkout" }
 	];
 
 	const getPathItems = () => {
+		const currentPath = location.pathname.toLowerCase();
 		const items = [];
 		for (let i = 0; i < pathItems.length; i++) {
-			items.push(
-				<React.Fragment key={pathItems[i].name}>
-					{i > 0 && <p>/</p>}
-					{id === pathItems[i].name ? (
-						<p>{pathItems[i].name}</p>
-					) : (
-						<Link to={pathItems[i].path} className={pathItems[i].isHome ? s.home : undefined}>
-							{pathItems[i].name}
-						</Link>
-					)}
-				</React.Fragment>
-			);
+			const { name, path, isHome } = pathItems[i];
+			const isActive = currentPath.startsWith(path.toLowerCase());
+
+			if (isActive || name === "Home") {
+				items.push(
+					<React.Fragment key={name}>
+						{items.length > 0 && <p>/</p>}
+						{isActive || name === "Home" ? (
+							<Link
+								to={name === "Home" ? "/" : path}
+								className={name === "Home" ? s.home : undefined}>
+								{name}
+							</Link>
+						) : (
+							<Link to={path} className={isHome ? s.home : undefined}>
+								{name}
+							</Link>
+						)}
+					</React.Fragment>
+				);
+			}
 		}
 		return items;
 	};
