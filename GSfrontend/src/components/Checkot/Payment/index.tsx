af2c621836b2ca836bcad11debdceeaa07bec979
Khanaccount <khanaccount1@mail.ru>
2023-12-30 23:29:14 +0700

    Сделал старинцу оплаты, перенастроил рефрешь токен

--- GSfrontend/src/components/Checkot/Payment/index.tsx
+++ GSfrontend/src/components/Checkot/Payment/index.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
-import PhoneInput from "react-phone-number-input/input";
 import { Link } from "react-router-dom";
 import { getAuthHeaders } from "../../../api/auth";
+import Delete from "../../OrderQuantitySelector/svg/delete";
 import axios from "axios";
 
 import s from "./index.module.scss";
@@ -31,21 +31,31 @@ export interface OrderPrices {
 	isFreeDelivery: boolean;
 }
 
+interface ShippingAddress {
+	id: number;
+	state: string;
+	phone: string;
+	city: string;
+	region: string;
+	streetAddress: string;
+	lastName: string;
+	firstName: string;
+	customer: {
+		email: string;
+		username: string;
+		id: number;
+	};
+}
+
 export interface OrderInfo {
 	prices: OrderPrices;
 	output: OrderItem[];
 }
 
 const Payment: React.FC = () => {
-	const [phoneNumber, setPhoneNumber] = useState<string | undefined>(undefined);
-	const [showRegionDropdown, setShowRegionDropdown] = useState(false);
-	const [showStateDropdown, setShowStateDropdown] = useState(false);
-	const [filteredRegions, setFilteredRegions] = useState<string[]>([]);
-	const [selectedCountry, setSelectedCountry] = useState<string | undefined>(undefined);
-	const [filteredStates, setFilteredStates] = useState<string[]>([]);
-	const [showRegionDropdownImg, setShowRegionDropdownImg] = useState(false);
-	const [showStateDropdownImg, setShowStateDropdownImg] = useState(false);
 	const [selectedMethod, setSelectedMethod] = useState<number | null>(null);
+	const [selectedAddress, setSelectedAddress] = useState<number | null>(null);
+	const [shippingAddress, setShippingAddress] = useState<ShippingAddress[]>([]);
 	const [orderInfo, setOrderInfo] = useState<OrderInfo>({
 		prices: {
 			subtotalPrice: "",
@@ -58,10 +68,10 @@ const Payment: React.FC = () => {
 		},
 		output: []
 	});
-	console.log(orderInfo);
 
 	useEffect(() => {
 		fetchItems();
+		fetchShippingAddresses();
 	}, []);
 
 	const fetchItems = () => {
@@ -75,108 +85,16 @@ const Payment: React.FC = () => {
 				console.error("Error response:", error);
 			});
 	};
-
-	const handlePhoneChange = (value: string | undefined) => {
-		setPhoneNumber(value);
-	};
-
-	const toggleRegionDropdown = () => {
-		setShowRegionDropdown(!showRegionDropdown);
-		setShowStateDropdown(false);
-		setShowRegionDropdownImg(!showRegionDropdownImg);
-		setShowStateDropdownImg(false);
-		if (!showRegionDropdown) {
-			handleRegionInputChange({ target: { value: "" } } as React.ChangeEvent<HTMLInputElement>);
-		}
-	};
-
-	const toggleStateDropdown = () => {
-		setShowStateDropdown(!showStateDropdown && !!selectedCountry);
-		setShowRegionDropdown(false);
-		setShowStateDropdownImg(!showStateDropdownImg);
-		setShowRegionDropdownImg(false);
-		if (!showStateDropdown) {
-			handleStateInputChange({ target: { value: "" } } as React.ChangeEvent<HTMLInputElement>);
-		}
-	};
-
-	const handleRegionSelect = (selectedRegion: string) => {
-		const regionInput = document.getElementById("Region") as HTMLInputElement;
-		regionInput.value = selectedRegion;
-		setSelectedCountry(selectedRegion);
-		setShowRegionDropdown(false);
-		setShowStateDropdown(false);
-	};
-
-	const handleStateSelect = (selectedState: string) => {
-		const stateInput = document.getElementById("State") as HTMLInputElement;
-		stateInput.value = selectedState;
-		setShowStateDropdown(false);
-	};
-
-	const handleRegionInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-		const inputValue = event.target.value.toLowerCase();
-
-		type Country = {
-			id: number;
-			country: string;
-		};
-
-		const availableRegions: Country[] = [
-			{ id: 1, country: "USA" },
-			{ id: 2, country: "Russia" },
-			{ id: 3, country: "Germany" },
-			{ id: 4, country: "Japan" },
-			{ id: 5, country: "China" }
-		];
-
-		const filteredRegions = availableRegions
-			.filter((region) => region.country.toLowerCase().startsWith(inputValue))
-			.map((region) => region.country);
-
-		setFilteredRegions(filteredRegions);
-		setShowRegionDropdown(true);
-		setSelectedCountry(undefined);
-		setFilteredStates([]);
-		setShowStateDropdown(false);
-
-		if (!inputValue) {
-			handleRegionInputClear();
-		}
-	};
-
-	const handleRegionInputClear = () => {
-		const regionInput = document.getElementById("Region") as HTMLInputElement;
-		const stateInput = document.getElementById("State") as HTMLInputElement;
-
-		regionInput.value = "";
-		stateInput.value = "";
-		setSelectedCountry(undefined);
-		setShowStateDropdown(false);
-	};
-
-	const handleStateInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-		const inputValue = event.target.value.toLowerCase();
-
-		const statesByCountry: Record<string, string[]> = {
-			USA: ["New York", "California", "Texas", "Florida"],
-			Russia: ["Moscow", "Saint Petersburg", "Novosibirsk"],
-			Germany: ["Berlin", "Munich", "Hamburg"],
-			Japan: ["Tokyo", "Osaka", "Kyoto"],
-			China: ["Beijing", "Shanghai", "Guangzhou"]
-		};
-
-		const selectedStates = statesByCountry[selectedCountry || ""];
-
-		if (selectedStates && selectedCountry) {
-			const filteredStates = selectedStates.filter((state) =>
-				state.toLowerCase().startsWith(inputValue)
-			);
-			setFilteredStates(filteredStates);
-			setShowStateDropdown(true);
-		} else {
-			setShowStateDropdown(false);
-		}
+	const fetchShippingAddresses = () => {
+		const token = getAuthHeaders();
+		axios
+			.get("http://127.0.0.1:8000/shop/shippingAddress/", token)
+			.then((response) => {
+				setShippingAddress(response.data);
+			})
+			.catch((error) => {
+				console.error("Error fetching shipping address data: ", error);
+			});
 	};
 
 	const handleMethodClick = (methodNumber: number) => {
@@ -186,171 +104,74 @@ const Payment: React.FC = () => {
 			setSelectedMethod(methodNumber);
 		}
 	};
+	const handleAddressClick = (index: number) => {
+		setSelectedAddress(index === selectedAddress ? null : index);
+	};
+
+	const handleDeleteAddress = (addressIdToDelete: number) => {
+		const authHeaders = getAuthHeaders();
+		const requestData = {
+			shippingAddress: addressIdToDelete
+		};
+
+		axios
+			.delete("http://127.0.0.1:8000/shop/shippingAddress/", {
+				headers: {
+					Authorization: authHeaders?.headers?.Authorization
+				},
+				data: requestData
+			})
+			.then(() => {
+				console.log(`Shipping address with ID ${addressIdToDelete} deleted successfully`);
+				axios
+					.get("http://127.0.0.1:8000/shop/shippingAddress/", authHeaders)
+					.then((response) => {
+						setShippingAddress(response.data);
+					})
+					.catch((error) => {
+						console.error("Error fetching updated shipping addresses after deletion: ", error);
+					});
+			})
+			.catch((error) => {
+				console.error(`Error deleting shipping address with ID ${addressIdToDelete}:`, error);
+			});
+	};
 
 	return (
 		<div className={s.payment}>
 			<div className={s.addressBlock}>
 				<h5 className={s.billingAddress}>Billing Address</h5>
-				<div className={s.inputBlock}>
-					<form className={s.firstForm}>
-						<label htmlFor="firstName">
-							<p className={s.formText}>
-								First Name <span>*</span>
-							</p>
-							<input type="text" id="firstName" name="firstName" />
-						</label>
-					</form>
-					<form className={s.secondForm}>
-						<label htmlFor="lastName">
-							<p className={s.formText}>
-								Last Name <span>*</span>
-							</p>
-							<input type="text" id="lastName" name="lastName" />
-						</label>
-					</form>
-				</div>
+				<Link to={"/account"}>
+					<button className={s.newAdresses}>Add new addresses</button>
+				</Link>
+				<div className={s.addedAddresses}>
+					{shippingAddress.map((address, index) => (
+						<div onClick={() => handleAddressClick(index)} className={s.adderss} key={index}>
+							<img className={s.cyrcle0} src="/img/payment/cyrcle0.svg" alt="cyrcle" />
+							{selectedAddress === index && (
+								<img className={s.cyrcle1} src="/img/payment/cyrcle1.svg" alt="cyrcle" />
+							)}
+							<h3>Shipping Address {index + 1}</h3>
 
-				<div className={s.inputBlock}>
-					<form className={s.firstForm}>
-						<label htmlFor="Region">
-							<p className={s.formText}>
-								Country / Region <span>*</span>
-							</p>
-							<input
-								onChange={handleRegionInputChange}
-								onClick={toggleRegionDropdown}
-								placeholder="Select a country / region"
-								type="text"
-								id="Region"
-								name="Region"
-							/>
-							<img
-								className={showRegionDropdown ? s.arrdownActive : s.arrdown}
-								src="/img/address/arrdown.svg"
-								alt="arrdown"
-							/>
-						</label>
-						{showRegionDropdown && (
-							<div className={s.dropdownContent}>
-								<ul className={s.regionList}>
-									{filteredRegions.map((region, index) => (
-										<li key={index} onClick={() => handleRegionSelect(region)}>
-											{region}
-										</li>
-									))}
-								</ul>
+							<div>
+								<p className={s.addressField}>First Name: {address.firstName}</p>
+								<p className={s.addressField}>Last Name: {address.lastName}</p>
 							</div>
-						)}
-					</form>
-
-					<form className={s.secondForm}>
-						<label htmlFor="City">
-							<p className={s.formText}>
-								Town / City <span>*</span>
-							</p>
-							<input type="text" id="City" name="City" />
-						</label>
-					</form>
-				</div>
-
-				<div className={s.inputBlock}>
-					<form className={s.firstForm}>
-						<label htmlFor="Region">
-							<p className={s.formText}>
-								Street Address <span>*</span>
-							</p>
-							<input
-								placeholder="House number and street name"
-								type="text"
-								id="Region"
-								name="Region"
-							/>
-						</label>
-					</form>
-					<form className={s.secondForm}>
-						<label htmlFor="appartment">
-							<p className={s.formText}>
-								Appartment <span>*</span>
-							</p>
-							<input
-								placeholder="Appartment, suite, unit, etc. (optional)"
-								type="text"
-								id="appartment"
-								name="appartment"
-							/>
-						</label>
-					</form>
-				</div>
-
-				<div className={s.inputBlock}>
-					<form className={s.firstForm}>
-						<label htmlFor="State">
-							<p className={s.formText}>
-								State <span>*</span>
-							</p>
-							<input
-								onChange={handleStateInputChange}
-								onClick={toggleStateDropdown}
-								placeholder="Select a state"
-								type="text"
-								id="State"
-								name="State"
-								disabled={!selectedCountry}
-							/>
-							<img
-								className={showStateDropdown ? s.arrdownActive : s.arrdown}
-								src="/img/address/arrdown.svg"
-								alt="arrdown"
-							/>
-						</label>
-						{showStateDropdown && (
-							<div className={s.dropdownContent}>
-								<ul className={s.stateList}>
-									{filteredStates.map((state, index) => (
-										<li key={index} onClick={() => handleStateSelect(state)}>
-											{state}
-										</li>
-									))}
-								</ul>
+							<div>
+								<p className={s.addressField}>Country / Region: {address.region}</p>
+								<p className={s.addressField}>Town / City: span {address.city}</p>
+								<p className={s.addressField}>Street Address: {address.streetAddress}</p>
 							</div>
-						)}
-					</form>
-					<form className={s.secondForm}>
-						<label htmlFor="City">
-							<p className={s.formText}>
-								Town / City <span>*</span>
-							</p>
-							<input type="text" id="City" name="City" />
-						</label>
-					</form>
+							<div>
+								<p className={s.addressField}>State : {address.state}</p>
+								<p className={s.addressField}>Phone: {address.phone}</p>
+							</div>
+							<button onClick={() => handleDeleteAddress(address.id)}>
+								<Delete />
+							</button>
+						</div>
+					))}
 				</div>
-
-				<div className={s.inputBlock}>
-					<form className={s.firstForm}>
-						<label htmlFor="EmailAddress">
-							<p className={s.formText}>
-								Email address<span>*</span>
-							</p>
-							<input type="text" id="EmailAddress" name="EmailAddress" />
-						</label>
-					</form>
-					<form className={s.secondForm}>
-						<label htmlFor="PhoneNumber">
-							<p className={s.formText}>
-								Phone Number <span>*</span>
-							</p>
-							<PhoneInput
-								id="PhoneNumber"
-								name="PhoneNumber"
-								placeholder="Enter phone number"
-								value={phoneNumber}
-								onChange={handlePhoneChange}
-							/>
-						</label>
-					</form>
-				</div>
-
-				<h5 className={s.differentAddress}>Ship to a different address?</h5>
 				<p className={s.optional}>Order notes (optional)</p>
 				<form>
 					<textarea className={s.notes} id="textInput" name="textInput"></textarea>
@@ -413,11 +234,8 @@ const Payment: React.FC = () => {
 							alt="acceptMethods"
 						/>
 					</div>
-					<div className={s.method} onClick={() => handleMethodClick(2)}>
-						<img className={s.cyrcle0} src="/img/payment/cyrcle0.svg" alt="cyrcle" />
-						{selectedMethod === 2 && (
-							<img className={s.cyrcle1} src="/img/payment/cyrcle1.svg" alt="cyrcle" />
-						)}
+					<div className={s.methodNone}>
+						<p className={s.Cross}>x</p>
 
 						<h3>Cash on delivery</h3>
 					</div>
